1c51ddf197240019e7075574ad33949e304e30dc
Sander Danielsson <sander.danielsson@gmail.com>
2016-05-25 13:36:52 +0200

    Change view for single task

--- client/components/SingleTask.js
+++ client/components/SingleTask.js
@@ -1,11 +1,17 @@
 import React from 'react';
-
+import Task from './task';
 
 const SingleTask = React.createClass({
   render () {
+
+    const { taskId } = this.props.params;
+    const i = this.props.tasks.findIndex((task) => task.id === taskId);
+
+    const task = this.props.tasks[i];
+
     return (
       <div className="single-task">
-      This is one single task
+        <Task i={i} task={task} {...this.props} />
       </div>
     )
   }
