68cd2d39a943cf5cddf9f88a137b79af5157401e
sanderdan <sander.danielsson@gmail.com>
2016-05-30 09:28:22 +0200

    Remove materializecss. Add checkbox

--- client/components/CreateTask.js
+++ client/components/CreateTask.js
@@ -7,7 +7,8 @@ const CreateTask = React.createClass ({
 		e.preventDefault();
 		const title = this.refs.title.value;
 		const picture = this.refs.picture.value;
-		console.log("title: " + title + " picture: " + picture);
+		const isActive = this.refs.isActive.value;
+		console.log("title: " + title + " picture: " + picture + " isActive: " + isActive );
 		this.refs.taskForm.reset();
 	},
 
@@ -16,6 +17,7 @@ const CreateTask = React.createClass ({
 			<form onSubmit={this.handleSubmit} ref="taskForm">
 				<input type="text" ref="title" placeholder="Title" />
 				<input type="text" ref="picture" placeholder="Image url" />
+				<input type="checkbox" ref="isActive" />
 				<input type="submit" hidden />
 			</form>
 
